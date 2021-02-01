8b11fd790e2e541a9ebd4b42e551b6c4a3ea0c5f
kubrasnmez <59183453+kubrasnmez@users.noreply.github.com>
2021-02-01 21:48:18 +0300

    ReCapProject

---
+++ ReCapProject/Business/Concreate/CarManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concreate
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+        public List<Car> GetAll()
+        {
+            //İş Kodları
+            return _carDal.GetAll();
+        }
+    }
+}
---
+++ ReCapProject/DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Dal - Data Access Layer
+    public interface ICarDal
+    {
+        List<Car> GetAll();
+
+        void Add(Car car);
+        void Update(Car car);
+        void Delete(Car car);
+
+        List<Car> GetAllByBrand(int brandId);
+    }
+}
